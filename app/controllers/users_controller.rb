d998e93730fbb7c27e19b43d5834692a6f4ab600
eito1011 <eito.5.5855@gmail.com>
2021-08-31 16:11:19 +0900

    mypageのroute開通

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,6 @@ class UsersController < ApplicationController
   end
 
   def me
-    binding.pry
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'mypage', to: 'user#me'
+  get 'mypage', to: 'users#me'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   root 'home#index'
