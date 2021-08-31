804934fa41311373e83679d2c8269a6178652b66
eito1011 <eito.5.5855@gmail.com>
2021-08-31 15:44:21 +0900

    ログインフォームの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,24 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       redirect_to mypage_path
-    else 
-      flash[:user] = user
-      flash[:error_messages] = user.errors.full_messages
+    else
+      flash[:alert] = 'ユーザー登録できませんでした'
+      # flash[:user] = user
+      # flash[:error_messages] = user.errors.full_messages
       redirect_back fallback_location: 'http://localhost'
     end
+
+# memoriesのcreate action
+    # if @memory.save
+    #   redirect_to root_path, notice: '登録が完了しました!'
+    # else
+    #   flash[:alert] = '登録が完了できませんでした'
+    #   render :new
+    # end     
   end
 
   def me
+    binding.pry
   end
 
   private
