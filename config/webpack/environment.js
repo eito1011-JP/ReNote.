139f01fd48411814c635ff91b5b8b2698b9b7649
eito <eito.55855@gmail.com>
2021-12-16 22:25:43 +0900

    ec2との整合性の確認

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,21 @@
-const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+// const { environment } = require('@rails/webpacker')
+// const { VueLoaderPlugin } = require('vue-loader')
+// const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
+// environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+// environment.loaders.prepend('vue', vue)
+// module.exports = environment
 
-const webpack = require('webpack')
-  environment.plugins.prepend(
-    'Provide',
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      Popper: 'popper.js'
-    })
-  )
+// const webpack = require('webpack')
+//   environment.plugins.prepend(
+//     'Provide',
+//     new webpack.ProvidePlugin({
+//       $: 'jquery',
+//       jQuery: 'jquery',
+//       Popper: 'popper.js'
+//     })
+//   )
 
+  const { environment } = require('@rails/webpacker')
 
+  module.exports = environment
