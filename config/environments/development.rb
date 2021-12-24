5ca310e8c61494135938f519b2c526f799e52f51
eito <eito.55855@gmail.com>
2021-12-24 16:38:59 +0900

    config/environments/development.rbの変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
@@ -77,3 +79,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
 end
+
+
+##ここから
